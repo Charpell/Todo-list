4a864d445109da176da2a9571f41a19b0c7455d4
Charpell <charpellumeh@gmail.com>
2017-05-02 05:15:33 -0700

    Adding todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,33 @@
 // Check off Specific Todos By Clicking
-$('li').click(function(){
+
+$('ul').on("click", "li", function(){
 	$(this).toggleClass("completed")
 
 });
 
+// $('li').on(function(){
+// 	$(this).toggleClass("completed")
+
+// });
+
 //Click on X to Delete Todo
-$("span").click(function(event){ // event prevents propagation
+//$("span").click(function(event){ 
+$("ul").on("click", "span", function(event){ // event prevents propagation
 	$(this).parent().fadeOut(500, function(){ //removes the entire li
 		$(this).remove();
 	});  //removes the entire li
 	event.stopPropagation();
 
+})
+
+
+// Add a new todo list 
+$('input[type="text"').keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todotext = $(this).val();
+		$(this).val("");
+		//create a new li and add tp ul
+		$('ul').append("<li><span>X</span> "+ todotext +"</li>")
+	}
 })
