33bfdab3c26dfc32c05d20c089bcedeec86e80d2
Charpell <charpellumeh@gmail.com>
2017-05-02 04:57:11 -0700

    Deleting

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,12 @@ $('li').click(function(){
 	$(this).toggleClass("completed")
 
 });
+
+//Click on X to Delete Todo
+$("span").click(function(event){ // event prevents propagation
+	$(this).parent().fadeOut(500, function(){ //removes the entire li
+		$(this).remove();
+	});  //removes the entire li
+	event.stopPropagation();
+
+})
